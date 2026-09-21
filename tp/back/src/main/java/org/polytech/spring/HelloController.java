d05486d41c240fa32638573c123b5793015a1ff7
MalakRai <malak.rai.78@gmail.com>
2026-09-21 11:54:47 +0200

    controller

---
+++ tp/back/src/main/java/org/polytech/spring/HelloController.java
@@ -0,0 +1,17 @@
+package org.polytech.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+       // return "Helllo";
+
+       return new Patient(id:0, nom: "Malak");
+
+    }
+
+}
--- tp/back/src/main/java/org/polytech/spring/Patient.java
+++ tp/back/src/main/java/org/polytech/spring/Patient.java
@@ -2,9 +2,14 @@ package org.polytech.spring;
 
 public class Patient {
 
-    
+    private String nom;
 
 
-
+    public Patient(String nom){
+        this.nom=nom;
+    }
+    public String getNom(String nom){
+        return nom;
+    }
 
 }
