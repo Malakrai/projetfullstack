b1ace06711963a8d5de28894c26215b0ec5e9fa4
MalakRai <malak.rai.78@gmail.com>
2026-09-21 11:22:50 +0200

    lancement springboot

--- tp/back/src/main/java/org/polytech/spring/AppConfig.java
+++ tp/back/src/main/java/org/polytech/spring/AppConfig.java
@@ -1,5 +1,28 @@
 package org.polytech.spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
 
+    // AppConfig = la classe qui configure Spring et lui explique quels objets créer et comment les relier.
+    
+
+    // Quand quelqu’un demande un PatientStore, utilise cet objet PatientDataBase.
+    @Bean
+    public PatientStore PatientStore(){
+        return new PatientDataBase();
+    }
+
+    @Bean
+    public PatientService patientservice(PatientStore patientstore){
+        return new PatientService(patientstore);
+    }
+
 }
+
+
+//explications
+// Une méthode = comportement interne de la classe.
+//Une dépendance = autre objet dont la classe a besoin pour travailler.
--- tp/back/src/main/java/org/polytech/spring/PatientService.java
+++ tp/back/src/main/java/org/polytech/spring/PatientService.java
@@ -2,4 +2,10 @@ package org.polytech.spring;
 
 public class PatientService {
 
+    public PatientStore patientstore;
+
+    public PatientService(PatientStore patientstore){
+        this.patientstore = patientstore;
+    }
+
 }
